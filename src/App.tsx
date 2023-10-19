eace8857d012ecd46c6b293878da7176628ef450
FTAndy <fortheandy@gmail.com>
2023-10-19 21:42:03 +0800

    add: app bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,47 @@
-import './App.css';
-import Proflie from './pages/profile'
+import { useState } from 'react'
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
 import { Outlet } from "react-router-dom";
+import Link from '@mui/material/Link';
+import './App.scss'
+
+
+const pages = [
+  {
+    name: 'Home',
+    path: '/'
+  },
+  {
+    name: 'Comedians',
+    path: '/comedians'
+  },
+  {
+    name: 'Profile',
+    path: '/profile'
+  },
+  {
+    name: 'Forum',
+    path: '/forrm'
+  }
+]
 
 
 function App() {
   return (
     <div className="App">
+      <AppBar className='app-bar' position="fixed">
+        <Toolbar>
+          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+            {pages.map((page) => (
+              <Link className='link' href={page.path} underline="none">
+                {page.name}
+              </Link>            
+            ))}
+          </Box>
+          {/* <Button color="inherit">Login</Button> */}
+        </Toolbar>        
+      </AppBar>
       <Outlet />
     </div>
   );
